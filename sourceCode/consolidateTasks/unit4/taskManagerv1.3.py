eee879cd71350b49e023e395e2bcb5777582399e
Paul Sant <45962682+psant01@users.noreply.github.com>
2025-10-17 14:42:14 +0100

    New version of source code with deletion task implemented.

---
+++ sourceCode/consolidateTasks/unit4/taskManagerv1.3.py
@@ -0,0 +1,66 @@
+# A simple python program to act as a task manager
+# The program makes use of input, conditional and control structures
+
+def listAdditem(listOfTasks, taskName, TaskCreationDate, taskDueDate):
+    listOfTasks.append([taskName, taskCreationDate, taskDueDate])
+    return listOfTasks
+    
+def listRemoveitem(listOfTasks, itemNameToRemove, CreationDate, DueDate):
+    listOfTasks.remove([itemNameToRemove, CreationDate, DueDate])
+    return listOfTasks
+    
+userName = "paul"
+pwd = "L3tM31n"
+
+usernameInput = input("Please enter your username: ")
+userPwdInput = input("Please enter your password: ")
+
+while((usernameInput != userName and userPwdInput != pwd)):
+    print("sorry, incorrect username or password entered. Please try again")
+    usernameInput = input("Please enter your username: ")
+    userPwdInput = input("Please enter your password: ")
+
+# Create an empty list
+
+taskList = []
+
+# Begin with a welcome message
+
+print("Welcome to the Task Manager, what would you like to do? ")
+
+print("1. Add a new task")
+print("2. Update and existing task in your list")
+print("3. Delete a task from your current list")
+print("4. Print your list of tasks")
+print("Enter exit to leave")
+
+userChoice = input("Please enter which option you would like")
+
+while(userChoice != "exit" or userChoice != "Exit"):
+    # Check option choice and then execute correct function
+    if(userChoice == "1"):
+        taskName = input("Please add a task name: ")
+        taskCreationDate = input("Please add a creation date: ")
+        taskDueDate = input("Please add a due date: ")
+        taskList = listAdditem(taskList, taskName, taskCreationDate, taskDueDate)
+        print("The new list is: ")
+        print(taskList)
+        userChoice = input("What would you like to do next? ")
+    elif(userChoice == "2"):
+        taskName = input("Please state which task you wish to update")
+    elif(userChoice == "3"):
+        taskName = input("Please state which task you wish to delete.")
+        taskCreationDate = input("Please add a creation date: ")
+        taskDueDate = input("Please add a due date: ")
+        taskList = listRemoveitem(taskList,taskName, taskCreationDate, taskDueDate)
+        
+        print("The newly updated task list is: ")
+        print(taskList) # Print updated task list
+        
+        userChoice = input("What would you like to do next? ")
+    elif(userChoice == "4"):
+        print("The tasks in your current list are:")
+    elif(userChoice == "exit" or userChoice == "Exit"):
+        print("Goodbye, and thank you for using the talk list manager.")
+    else:
+        print("Sorry, invalid choice entered, exiting the program...")
